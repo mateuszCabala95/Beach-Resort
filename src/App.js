e3d5cf15f4185e258dfd00f35ae55ec894975d2a
Mateusz <mateusz.cabala95@gmail.com>
2020-02-20 19:58:09 +0100

    create routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {Link, Route, Switch} from 'react-router-dom'
+
 import "./App.css"
 
 
+import Home from "./pages/Home/Home";
+import Rooms from "./pages/Rooms/Rooms";
+import SingleRoom from "./pages/SingleRoom/SingleRoom";
+import Error from "./pages/Error/Error";
+
+
 function App() {
-  return (
-    <div>
-     hello from app
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/rooms/' component={Rooms}/>
+                <Route exact path='/rooms/:slug' component={SingleRoom}/>
+                <Route component={Error}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
